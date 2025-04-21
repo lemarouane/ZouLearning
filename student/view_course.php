bfb2638084d0c3831aea1814335675d3ebd930bd
lemarouane <marouane.haddad@esi.ac.ma>
2025-04-21 01:03:16 +0100

    Icon

--- student/view_course.php
+++ student/view_course.php
@@ -59,6 +59,8 @@ $stmt->execute();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Voir Cours - Zouhair E-Learning</title>
+    <link rel="icon" type="image/png" href="../assets/img/logo.png">
+
     <link rel="stylesheet" href="../assets/css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
--- student/view_quiz.php
+++ student/view_quiz.php
@@ -102,6 +102,8 @@ $stmt->execute();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Voir Quiz - Zouhair E-Learning</title>
+    <link rel="icon" type="image/png" href="../assets/img/logo.png">
+
     <link rel="stylesheet" href="../assets/css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
